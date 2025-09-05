fdf3cbddcba85265c480dc27036fec30b461d406
azaria amanda <azaryaamanda@gmail.com>
2025-09-05 07:02:35 +0700

    Percobaan 1

---
+++ Praktikum_PBO/Jobsheet2/MahasiswaDemo05.java
@@ -0,0 +1,11 @@
+public class MahasiswaDemo05 {
+    public static void main(String[] args) {
+        Mahasiswa05 m1 = new Mahasiswa05();
+        m1.nim = "023432";
+        m1.nama = "Yansy Ayuningtias";
+        m1.alamat = "Nias, Sumatra Utara";
+        m1.kelas = "2A";
+
+        m1.displayBiodata();
+    }
+}
