37a6822108ec06bd307d07e1c92440ae3a9c9945
azaria amanda <azaryaamanda@gmail.com>
2025-09-12 08:23:16 +0700

    modif

--- Praktikum_PBO/Jobsheet3/MotorDemo04.java
+++ Praktikum_PBO/Jobsheet3/MotorDemo04.java
@@ -4,18 +4,43 @@ public class MotorDemo04 {
         motor1.displayStatus();
 
         motor1.platNomor = "B 0838 XZ";
-        motor1.kecepatan = 50;
+
+        int kecepatanBaru = 50;
+
+        if (!motor1.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        }
+        else{
+            motor1.kecepatan = kecepatanBaru;
+        }
+
         motor1.displayStatus();
 
         Motor04 motor2 = new Motor04();
         motor2.platNomor = "N 9840 AB";
         motor2.isMesinOn = true;
         motor2.kecepatan = 40;
+
+        if (!motor2.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        }
+        else{
+            motor2.kecepatan = kecepatanBaru;
+        }
+
         motor2.displayStatus();
 
         Motor04 motor3 = new Motor04();
         motor3.platNomor = "D 8343 CV";
         motor3.kecepatan = 60;
+
+        if (!motor3.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        }
+        else{
+            motor3.kecepatan = kecepatanBaru;
+        }
+
         motor3.displayStatus();
 
     }
