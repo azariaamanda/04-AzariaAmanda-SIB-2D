21c143ae06b90da12999f501ebbe1d3a60e6b52b
azaria amanda <azaryaamanda@gmail.com>
2025-09-22 20:05:57 +0700

    Praktikum 3 Soal 3.2

--- Praktikum_PemrogramanWeb/Jobsheet4_PHP/operator.php
+++ Praktikum_PemrogramanWeb/Jobsheet4_PHP/operator.php
@@ -18,3 +18,19 @@ echo "Hasil Kali: {$hasilKali} <br>";
 echo "Hasil Bagi: {$hasilBagi} <br>";
 echo "Sisa Bagi: {$sisaBagi} <br>";
 echo "Pangkat: {$pangkat} <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "<br>";
+echo "Hasil Sama: {$hasilSama} <br>";
+echo "Hasil Tidak Sama: {$hasilTidakSama} <br>";
+echo "Hasil Lebih Kecil: {$hasilLebihKecil} <br>";
+echo "Hasil Lebih Besar: {$hasilLebihBesar} <br>";
+echo "Hasil Lebih Kecil Sama: {$hasilLebihKecilSama} <br>";
+echo "Hasil Lebih Besar Sama: {$hasilLebihBesarSama} <br>";
+
