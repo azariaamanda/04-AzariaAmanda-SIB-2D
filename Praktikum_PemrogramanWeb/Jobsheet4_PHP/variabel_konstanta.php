a457203d8d9a1616f67331dc336c52c66413313d
azaria amanda <azaryaamanda@gmail.com>
2025-09-22 10:33:02 +0700

    Praktikum 1

--- Praktikum_PemrogramanWeb/Jobsheet4_PHP/variabel_konstanta.php
+++ Praktikum_PemrogramanWeb/Jobsheet4_PHP/variabel_konstanta.php
@@ -3,10 +3,12 @@ $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+echo "<br>";
 
 $benar = true;
 $salah = false;
-echo "Variabel benat: $benar, Variabel salah: $salah";
+echo "Variabel benar: $benar, Variabel salah: $salah";
+echo "<br>";
 
 //Mendefinisikan konstanta unruk nikai tetap
 define("NAMA_SITUS", "WebsiteKu.com");
