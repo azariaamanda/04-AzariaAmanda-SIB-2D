0309790189397a971109d18e72b5ffe69c16121b
azaria amanda <azaryaamanda@gmail.com>
2025-09-28 22:39:42 +0700

    Praktikum 5

--- Praktikum_PemrogramanWeb/JS05_PHP-2/recursive.php
+++ Praktikum_PemrogramanWeb/JS05_PHP-2/recursive.php
@@ -1,8 +1,12 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
+// function tampilkanHaloDunia(){
+//     echo "Halo dunia! <br>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+// tampilkanHaloDunia();
+
+for (si=1; si <= 25; si++){
+    echo "Perulangan ke-{si} <br>";
 }
-tampilkanHaloDunia();
 ?>
