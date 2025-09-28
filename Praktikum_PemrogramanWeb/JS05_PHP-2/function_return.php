7a1f707a7399536b701e2a973d832d7bf0e9338c
azaria amanda <azaryaamanda@gmail.com>
2025-09-28 22:25:12 +0700

    Praktikum 4 No 8

--- Praktikum_PemrogramanWeb/JS05_PHP-2/function_return.php
+++ Praktikum_PemrogramanWeb/JS05_PHP-2/function_return.php
@@ -1,8 +1,19 @@
 <?php
-function hitungUmur($thn_lahir, $thn_sekarang) {
-    $umur = $thn_sekarang - $thn_lahir;
-    return $umur;
-}
+    function hitungUmur($thn_lahir, $thn_sekarang){
+        $umur = $thn_sekarang - $thn_lahir;
+        return $umur;
+    }
 
-echo "Umur saya adalah ". hitungUmur(2006, 2023) . " tahun"
+    //fungsi perkenalan
+    function perkenalan($nama, $salam="Assalamualaikum"){
+        echo $salam.",";
+        echo "Perkenalkan, nama saya ".$nama."<br>";
+
+        //memanggil fungsi lain
+        echo "Saya berusia ". hitungUmur(1988, 2023) . " tahun<br>";
+        echo "Senang berkenalan dengan anda<br>";
+    }
+
+    //memanggil fungsi perkenalan
+    perkenalan ("Elok");
 ?>
