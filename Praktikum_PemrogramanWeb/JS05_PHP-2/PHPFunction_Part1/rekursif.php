3f05165edb8709ff294c263f6b5bd653233f6a8e
azaria amanda <azaryaamanda@gmail.com>
2025-09-30 18:57:11 +0700

    No 6

--- Praktikum_PemrogramanWeb/JS05_PHP-2/PHPFunction_Part1/rekursif.php
+++ Praktikum_PemrogramanWeb/JS05_PHP-2/PHPFunction_Part1/rekursif.php
@@ -1,12 +1,11 @@
 <?php
-// function tampilkanHaloDunia(){
-//     echo "Halo dunia! <br>";
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
 
-//     tampilkanHaloDunia();
-// }
-// tampilkanHaloDunia();
-
-for ($i=1; $i <= 25; $i++){
-    echo "Perulangan ke-{$i} <br>";
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
+tampilkanAngka(20);
 ?>
