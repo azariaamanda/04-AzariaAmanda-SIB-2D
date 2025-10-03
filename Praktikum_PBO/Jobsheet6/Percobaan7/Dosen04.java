1602bd50f4e31ebb5b74d54c95f0c2bc1301234b
azaria amanda <azaryaamanda@gmail.com>
2025-10-03 10:29:27 +0700

    selesai

--- Praktikum_PBO/Jobsheet6/Percobaan7/Dosen04.java
+++ Praktikum_PBO/Jobsheet6/Percobaan7/Dosen04.java
@@ -4,17 +4,13 @@ public class Dosen04 extends Pegawai04  {
     public String nidn;
 
     public Dosen04(String nip, String nama, double gaji, String nidn){
-        System.out.println("Dengan Parameter");
-        super();
-        super.nip = nip;
-        super.nama = nama;
-        super.gaji = gaji;
+        super(nip, nama, gaji);
         this.nidn = nidn;
     }
 
-    public Dosen04() {
-        System.out.println("Objek dari class Dosen dibuat");
-    }
+    // public Dosen04() {
+    //     System.out.println("Objek dari class Dosen dibuat");
+    // }
 
     public String getInfo() {
         return "NIDN        : " + this.nidn + "\n";
--- Praktikum_PBO/Jobsheet6/Percobaan7/Pegawai04.java
+++ Praktikum_PBO/Jobsheet6/Percobaan7/Pegawai04.java
@@ -5,8 +5,14 @@ public class Pegawai04 {
     public String nama;
     public double gaji;
 
-    public Pegawai04() {
-        System.out.println("Objek dari class Pegawai dibuat.");
+    // public Pegawai04() {
+    //     System.out.println("Objek dari class Pegawai dibuat.");
+    // }
+
+    public Pegawai04 (String nip, String nama, double gaji) {
+        this.nip = nip;
+        this.nama = nama;
+        this.gaji = gaji;
     }
 
     public String getInfo() {
