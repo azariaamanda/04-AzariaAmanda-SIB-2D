288ebe0b0a7f13ac06348cbab56d1bc81639a93a
azaria amanda <azaryaamanda@gmail.com>
2025-10-03 08:44:37 +0700

    Jobsheet6

---
+++ Praktikum_PBO/Jobsheet6/Percobaan1/Pegawai04.java
@@ -0,0 +1,8 @@
+package Percobaan1;
+
+public class Pegawai04 {
+
+    public Pegawai04() {
+        System.out.println("Objek dari class Pegawai dibuat.");
+    }
+}
---
+++ Praktikum_PBO/Jobsheet6/testpackage/InherintanceDemo04.java
@@ -0,0 +1,14 @@
+package testpackage;
+import Percobaan.Dosen04;
+public class InherintanceDemo04 {
+    public static void main(String[] args) {
+        Dosen04 dosen1 = new Dosen04();
+
+        dosen1.nama = "Yansy Ayuningtyas";
+        dosen1.nip = "34329837";
+        dosen1.gaji = 3000000;
+        dosen1.nidn = "1989432439";
+
+        System.out.println(dosen1.getInfo());
+    }
+}
