3df978ba478755e82ca8cd109a5f693e49d65604
azaria amanda <azaryaamanda@gmail.com>
2025-10-08 11:24:57 +0700

    selesai

---
+++ Praktikum_PBO/Jobsheet7/Percobaan1/InherintanceDemo04.java
@@ -0,0 +1,8 @@
+package Percobaan1;
+
+public class InherintanceDemo04 {
+    public static void main(String[] args) {
+        Dosen04 dosen1 = new Dosen04();
+    }
+
+}
---
+++ Praktikum_PBO/Jobsheet7/Percobaan2/Dosen04.java
@@ -0,0 +1,9 @@
+package Percobaan2;
+
+public class Dosen04 extends Pegawai04  {
+    public String nidn;
+
+    public Dosen04() {
+        System.out.println("Objek dari class Dosen dibuat");
+    }
+}
