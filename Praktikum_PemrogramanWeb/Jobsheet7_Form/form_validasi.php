01d281d51432fb5c78b3bb6649b70c8378ce518e
azaria amanda <azaryaamanda@gmail.com>
2025-10-13 15:58:45 +0700

    Praktikum 7 No 3

--- Praktikum_PemrogramanWeb/Jobsheet7_Form/form_validasi.php
+++ Praktikum_PemrogramanWeb/Jobsheet7_Form/form_validasi.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Form Input dengan Validasi</title>
+    <title>Form Input dengan Validasi dan AJAX</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    <h1>Form Input dengan Validasi</h1>
-    <form id="myForm" action="proses_validasi.php" method="post">
+    <h1>Form Input dengan Validasi dan AJAX</h1>
+    <form id="myForm">
         <label for="nama">Nama:</label>
         <input type="text" name="nama" id="nama">
         <span id="nama-error" style="color: red;"></span><br>
@@ -18,9 +18,12 @@
         <input type="submit" value="Submit">
     </form>
 
+    <div id="response" style="margin-top: 10px; color: green;"></div>
+
     <script>
         $(document).ready(function(){
             $("#myForm").submit(function(event){
+                event.preventDefault();
                 var nama = $("#nama").val();
                 var email = $("#email").val();
                 var valid = true;
@@ -39,8 +42,18 @@
                     $("#email-error").text("");
                 }
 
-                if (!valid){
-                    event.preventDefault();
+                if(valid){
+                    $.ajax({
+                        url: "proses_validasi.php",
+                        method: "POST",
+                        data: { nama: nama, email: email },
+                        success: function(response){
+                            $("#response").html(response);
+                        },
+                        error: function(){
+                            $("#response").text("Terjadi kesalahan saat mengirim data.");
+                        }
+                    });
                 }
             });
         });
