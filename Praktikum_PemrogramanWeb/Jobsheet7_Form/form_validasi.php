398cfca1eea42dc102d258add699ea6a29ac5ee9
azaria amanda <azaryaamanda@gmail.com>
2025-10-13 15:44:38 +0700

    Praktikum 7 No 1

---
+++ Praktikum_PemrogramanWeb/Jobsheet7_Form/form_validasi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Input dengan Validasi</title>
+</head>
+<body>
+    <h1>Form Input dengan Validasi</h1>
+    <form action="proses_validasi.php" method="post">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama">
+        <br>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+        <br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
