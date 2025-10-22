ce89bf3535fbcdd064d435f9f7c7b14ec0d535e5
azaria amanda <azaryaamanda@gmail.com>
2025-10-23 03:11:41 +0700

    Praktikum 1 No 5

--- Praktikum_PemrogramanWeb/Jobsheet8/upload.php
+++ Praktikum_PemrogramanWeb/Jobsheet8/upload.php
@@ -2,11 +2,21 @@
     if(isset($_POST["submit"])){
         $targetdir = "uploads/";
         $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+        $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-            echo "File berhasil diunggah.";
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxsize = 5*1024*1024;
+
+        if(in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize){
+            if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+                echo "File berhasil diunggah.<br><br>";
+                echo "<b>Berikut gambar yang sudah terupload:</b><br>";
+                echo "<img src='$targetfile' width='200' style='height:auto; border:2px solid #ccc; border-radius:10px;'>";
+            } else {
+                echo "Gagal mengunggal file.";
+            }
         } else {
-            echo "Gagal mengunggal file.";
+            echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
         }
     }
 ?>
