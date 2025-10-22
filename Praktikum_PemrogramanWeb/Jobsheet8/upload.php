de36e4b53f8e33399dc2e216877ad8c0e9ea9a9f
azaria amanda <azaryaamanda@gmail.com>
2025-10-23 02:32:34 +0700

    Praktikum 1 No 1

--- Praktikum_PemrogramanWeb/Jobsheet8/upload.php
+++ Praktikum_PemrogramanWeb/Jobsheet8/upload.php
@@ -1,11 +1,12 @@
 <?php
-if(isset($_POST["submit"])){
-    $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]){
-        echo "File berhasil diunggah.";
+    if(isset($_POST["submit"])){
+        $targetdir = "uploads/";
+        $targetfile = $targetdir . basename($_FILES["myFile"]["name"]);
+
+        if(move_uploaded_file($_FILES["myFile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggal file.";
+        }
     }
-    else{
-        echo "Gagal mengunggah file.";
-    }
-}
 ?>
