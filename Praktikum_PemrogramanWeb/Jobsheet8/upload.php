b5d2949c283acf4ebf4feb7dbcdd5530a4ed2ee8
azaria amanda <azaryaamanda@gmail.com>
2025-10-23 03:16:55 +0700

    Praktikum 1 No 6 & 7

--- Praktikum_PemrogramanWeb/Jobsheet8/upload.php
+++ Praktikum_PemrogramanWeb/Jobsheet8/upload.php
@@ -4,14 +4,12 @@
         $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
         $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-        $maxsize = 5*1024*1024;
+        $allowedExtensions = array("txt", "pdf", "doc", "docx");
+        $maxsize = 3*1024*1024;
 
         if(in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize){
             if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
                 echo "File berhasil diunggah.<br><br>";
-                echo "<b>Berikut gambar yang sudah terupload:</b><br>";
-                echo "<img src='$targetfile' width='200' style='height:auto; border:2px solid #ccc; border-radius:10px;'>";
             } else {
                 echo "Gagal mengunggal file.";
             }
