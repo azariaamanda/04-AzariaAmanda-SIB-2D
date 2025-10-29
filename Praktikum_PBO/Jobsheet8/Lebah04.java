2520b6c6af8c629ced9d376266f8522d425fbb76
azaria amanda <azaryaamanda@gmail.com>
2025-10-30 03:43:00 +0700

    Praktikum 4

--- Praktikum_PBO/Jobsheet8/Lebah04.java
+++
@@ -1,8 +0,0 @@
-public class Lebah04 extends Hewan04 {
-    public String kasta;
-
-    public Lebah04(String kasta, double berat, String makanan, String habitat){
-        super(berat, makanan, habitat);
-        this.kasta = kasta;
-    }
-}
