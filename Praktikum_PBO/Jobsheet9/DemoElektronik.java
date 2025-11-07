9d1bc4a87f8294f8e0903447447f74fedb19997c
azaria amanda <azaryaamanda@gmail.com>
2025-11-07 07:20:32 +0700

    Jobsheet 9

---
+++ Praktikum_PBO/Jobsheet9/DemoElektronik.java
@@ -0,0 +1,16 @@
+public class DemoElektronik {
+    public static void main(String[] args) {
+        Kipas kipas = new Kipas("blower", 1000000, "hitam", "politron");
+        TV tv = new TV("LED", 6, 70000000, "pink", "Samsung");
+        Kulkas kulkas = new Kulkas(2, 1000000, "merah", "Sharp");
+        SmartFridge smartFridge = new SmartFridge(9, 2, 60000000, "Silver", "politron");
+
+        System.out.println(kipas.getInfo());
+        System.out.println();
+        System.out.println(tv.getInfo());
+        System.out.println();
+        System.out.println(kulkas.getInfo());
+        System.out.println();
+        System.out.println(smartFridge.getInfo());
+    }
+}
