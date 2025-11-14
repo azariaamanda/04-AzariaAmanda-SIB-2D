c4381afb27bfa3334661ba3d367d1340c944192a
azaria amanda <azaryaamanda@gmail.com>
2025-11-14 10:07:43 +0700

    Full Bootstrap & CRUD

--- delete.php
+++ delete.php
@@ -20,3 +20,4 @@ try {
     http_response_code(500);
     echo 'Gagal menghapus: ' . htmlspecialchars($e->getMessage());
 }
+
--- index.php
+++ index.php
@@ -56,3 +56,4 @@ if (isset($_SESSION['user_id'])) {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
