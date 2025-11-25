d7464f0a036376f0aa7f3327d14e57d689837058
azaria amanda <azaryaamanda@gmail.com>
2025-11-25 17:03:10 +0700

    Object Casting no 5

--- Praktikum_PBO/Jobsheet10/Demo04.java
+++ Praktikum_PBO/Jobsheet10/Demo04.java
@@ -1,26 +1,15 @@
-import java.util.ArrayList;
+
 
 public class Demo04 {
     public static void main(String[] args) {
         Dosen04 dosen1 = new Dosen04("19940201", "Widia, S.Kom. M.Kom", "199402");
 
-        System.out.println(dosen1.nip);
-        System.out.println(dosen1.nama);
-        System.out.println(dosen1.nidn);
-        dosen1.mengajar();
+        Pegawai04 pegawai1 = dosen1;
+
+        System.out.println(pegawai1.nip);
+        System.out.println(pegawai1.nama);
+        System.out.println(pegawai1.nidn);
+        pegawai1.mengajar();
 
     }
 }
-
-// Dosen04 dosen2 = new Dosen04("19700105", "Muhammad, S.T, M.T", "197001");
-// TenagaKependidikan04 tendik1 = new TenagaKependidikan04("19750301", "Aida, A.Md.", "Tenaga Administrasi");
-// TenagaKependidikan04 tendik2 = new TenagaKependidikan04("19650304", "Rika, S.T.", "Tenaga Laboratorium");
-
-// ArrayList<Pegawai04> daftarPegawai = new ArrayList<Pegawai04>();
-
-// daftarPegawai.add(dosen1);
-// daftarPegawai.add(dosen2);
-// daftarPegawai.add(tendik1);
-// daftarPegawai.add(tendik2);
-
-// System.out.println("Jumlah Pegawai: " + daftarPegawai.size());
